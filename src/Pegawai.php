b036a332b705ec893a6551fbd17265fb52e8483c
Ardhan <ardhan.matematika@gmail.com>
2020-11-05 12:22:33 +0700

    melengkapi pegawai

--- src/Pegawai.php
+++ src/Pegawai.php
@@ -10,21 +10,68 @@ class Pegawai extends MainService
     private $kode_unit = '';
     private $jenis_pegawai = '';
     private $status_pegawai = '';
-    private $limit = '';
-    private $offset = '';
+    private $limit = '10';
+    private $offset = '0';
 
+
+    /**
+     * @param string $search
+     * @return $this
+     */
     public function search($search = '')
     {
         $this->search = $search;
         return $this;
     }
 
+
+    /**
+     * @param $kode_unit_induk
+     * @return $this
+     */
     public function kodeUnitInduk($kode_unit_induk)
     {
         $this->kode_unit_induk = $kode_unit_induk;
         return $this;
     }
 
+
+    /**
+     * @param $kode_unit
+     * @return $this
+     */
+    public function kodeUnit($kode_unit)
+    {
+        $this->kode_unit = $kode_unit;
+        return $this;
+    }
+
+
+    /**
+     * @param $jenis_pegawai
+     * @return $this
+     */
+    public function jenisPegawai($jenis_pegawai)
+    {
+        $this->jenis_pegawai = $jenis_pegawai;
+        return $this;
+    }
+
+
+    /**
+     * @param $status_pegawai
+     * @return $this
+     */
+    public function statusPegawai($status_pegawai)
+    {
+        $this->status_pegawai = $status_pegawai;
+        return $this;
+    }
+
+
+    /**
+     * @return false
+     */
     public function get()
     {
         $uri = $this->main_uri."/api/kepegawaian/pegawai/getdata?access_token=".$this->getToken();
@@ -46,5 +93,11 @@ class Pegawai extends MainService
         ];
 
         return $this->requestData($uri, $body, $headers);
+    }
+
+    public function first()
+    {
+        $data = $this->get();
+        return $data[0];
     }
 }
